70cd938de3cb1c013f4a286a5e5d783f93b7b23a
hongleang <lim.hongleang@gmail.com>
2021-08-18 21:38:39 +1000

    install webpack and configuration

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })]
+};
